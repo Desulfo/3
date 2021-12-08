f7d458cf1ed8dcfecad3bdc34f077a73c0b435b8
desulfo <puyol_11@interia.pl>
2021-12-08 15:09:48 +0100

    Task 3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+
+import "./App.css";
+
+const quoteAPI =
+  "https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json";
+const getRandomIndex = (number) => {
+  const randomIndex = Math.floor(Math.random() * number);
+  return randomIndex;
+};
 
 function App() {
+  const [quoteList, setQuoteList] = useState(null);
+  const [displayedQuotes, setDisplayedQuotes] = useState(null);
+  const [currentQuoteIndex, setCurrentQuoteIndex] = useState(null);
+
+  const getNewQuote = () => {
+    setDisplayedQuotes((prevQuotes) => [
+      ...prevQuotes,
+      quoteList[getRandomIndex(quoteList.length)],
+    ]);
+  };
+  useEffect(() => {
+    fetch(quoteAPI)
+      .then((response) => response.json())
+      .then((receivedQuoteList) => {
+        setQuoteList(receivedQuoteList);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+  useEffect(() => {
+    if (quoteList) {
+      setDisplayedQuotes([quoteList[getRandomIndex(quoteList.length)]]);
+      setCurrentQuoteIndex(0);
+    }
+  }, [quoteList]);
+  useEffect(() => {
+    if (displayedQuotes) {
+      setCurrentQuoteIndex(displayedQuotes.length - 1);
+    }
+  }, [displayedQuotes]);
+  const displayPrevQuote = () => {
+    setCurrentQuoteIndex((prevIndex) => prevIndex - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Your random qoute</h1>
+      {displayedQuotes && (
+        <figure>
+          <blockquote>{displayedQuotes[currentQuoteIndex].quote}</blockquote>
+          <figcaption>- {displayedQuotes[currentQuoteIndex].author}</figcaption>
+        </figure>
+      )}
+      <div className="buttonsWrapper">
+        <button onClick={getNewQuote}>New quote</button>
+        {currentQuoteIndex > 0 && (
+          <button onClick={displayPrevQuote}>Prev quote</button>
+        )}
+      </div>
+    </>
   );
 }
 
